a809361544851b7a4fe6f2fbe3514089fd1ff963
Simon Willison <swillison@gmail.com>
2021-07-28 18:10:22 -0700

    Test for "ttl" configuration

--- tests/test_remote_metadata.py
+++ tests/test_remote_metadata.py
@@ -1,5 +1,8 @@
+from time import sleep
 from datasette.app import Datasette
 import pytest
+
+TEST_URL = "https://www.example.com/metadata.yml"
 
 
 @pytest.fixture
@@ -10,8 +13,24 @@ def non_mocked_hosts():
 @pytest.mark.asyncio
 async def test_remote_metadata(httpx_mock):
     httpx_mock.add_response(
-        url="https://www.example.com/metadata.yml",
+        url=TEST_URL,
         data=b"title: This is the remote metadata title",
+    )
+    datasette = Datasette(
+        [],
+        memory=True,
+        metadata={"plugins": {"datasette-remote-metadata": {"url": TEST_URL}}},
+    )
+    response = await datasette.client.get("/")
+    assert response.status_code == 200
+    assert ">This is the remote metadata title<" in response.text
+
+
+@pytest.mark.asyncio
+async def test_ttl(httpx_mock):
+    httpx_mock.add_response(
+        url=TEST_URL,
+        data=b"title: Testing TTL",
     )
     datasette = Datasette(
         [],
@@ -19,11 +38,18 @@ async def test_remote_metadata(httpx_mock):
         metadata={
             "plugins": {
                 "datasette-remote-metadata": {
-                    "url": "https://www.example.com/metadata.yml"
+                    "url": TEST_URL,
+                    "ttl": 1,
                 }
             }
         },
     )
-    response = await datasette.client.get("/")
-    assert response.status_code == 200
-    assert ">This is the remote metadata title<" in response.text
+    await datasette.invoke_startup()
+    # More requests shouldn't trigger a metadata fetch
+    for _ in range(3):
+        await datasette.client.get("/")
+    assert len(httpx_mock.get_requests()) == 1
+    # Now wait a second and try again
+    sleep(1)
+    await datasette.client.get("/")
+    assert len(httpx_mock.get_requests()) == 2
